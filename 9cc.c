d63348423e72702a7f64cfa4dcc2ae4c863bfaaa
Naoto Takezawa <naoto9march@gmail.com>
2019-11-09 17:32:11 +0900

    乗除算をできるように変更

--- 9cc.c
+++ 9cc.c
@@ -22,6 +22,25 @@ struct Token {
 	char *str; // トークン文字列
 };
 
+// 抽象構文木のノードの種類
+typedef enum {
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木のノード
+struct Node {
+	NodeKind kind;
+	Node *lhs;
+	Node *rhs;
+	int val;
+};
+
 // 入力文字列
 char *user_input;
 
@@ -107,7 +126,7 @@ Token *tokenize(char *p) {
 				p++;
 				continue;
 			}
-			if(*p == '+' || *p == '-') {
+			if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
 				cur = new_token(TK_RESERVED, cur, p++);
 				continue;
 			}
@@ -123,6 +142,107 @@ Token *tokenize(char *p) {
 		return head.next;
 }
 
+// 2項演算子のノードを作成する
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs){
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs = lhs;
+	node->rhs = rhs;
+	return node;
+}
+
+// 整数のノードを作成する
+Node *new_node_num(int val){
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val = val;
+	return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+// 加減算
+Node *expr(){
+	Node *node = mul();
+
+	for(;;){
+		if (consume('+')) {
+			node = new_binary(ND_ADD, node, mul());
+		}
+		else if (consume('-')) {
+			node = new_binary(ND_SUB, node, mul());
+		}
+		else{
+			return node;
+		}
+	}
+}
+
+// 乗除算
+Node *mul() {
+	Node *node = primary();
+
+	for(;;) {
+		if (consume('*')) {
+			node = new_binary(ND_MUL, node, primary());
+		}
+		else if (consume('/')) {
+			node = new_binary(ND_DIV, node, primary());
+		}
+		else {
+			return node;
+		}
+	}
+}
+
+// 単項式
+Node *primary() {
+	if (consume('(')) {
+		Node *node = expr();
+		expect(')');
+		return node;
+	}
+	else {
+		// 今の文法では整数のはず
+		return new_node_num(expect_number());
+	}
+}
+
+// アセンブラコードを出力する
+void gen(Node *node) {
+	if (node->kind == ND_NUM) {
+		printf("  push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+	switch (node->kind) {
+		case ND_ADD:
+			printf("  add rax, rdi\n");
+			break;
+		case ND_SUB:
+			printf("  sub rax, rdi\n");
+			break;
+		case ND_MUL:
+			printf("  imul rax, rdi\n");
+			break;
+		case ND_DIV:
+			printf("  cqo\n");
+			printf("  idiv rdi\n");
+			break;
+	}
+
+	printf("  push rax\n");
+}
+
+
 int main(int argc, char **argv) {
 	if (argc != 2) {
 		fprintf(stderr, "引数の個数が正しくありません\n");
@@ -131,24 +251,19 @@ int main(int argc, char **argv) {
 
 	user_input = argv[1];
 	currentToken = tokenize(user_input);
+	Node *node = expr();
 
+	// アセンブリの前半部分を出力
 	printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-	// 最初は数値のはずなのでそれをmov命令へ出力
-  printf("  mov rax, %d\n", expect_number());
+	// アセンブリの本体を出力
+	gen(node);
 
-	while(!at_eof()) {
-		if (consume('+')) {
-			printf("  add rax, %d\n", expect_number());
-			continue;
-		}
-		// 今の仕様では+か-しか受け付けないので、+でなければ-であるはず
-		expect('-');
-		printf("  sub rax, %d\n", expect_number());
-	}
-
-  printf("  ret\n");
+	// スタックトップに式全体の値が残っているはずなので
+	// それをRAXにロードして関数からの返り値とする
+	printf("  pop rax\n");
+	printf("  ret\n");
 	return 0;
 }
